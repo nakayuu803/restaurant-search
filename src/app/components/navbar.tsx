cf7e7c3a995516a131bf9495191d8a25fc24d404
仲村 優佳 <ynakamura704@gmail.com>
2024-12-18 00:07:12 +0900

    ナビゲーションバーつけた

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar: React.FC = () => {
+    return (
+        <nav className = "bg-red-500 p-4">
+            <div className = "container mx-auto">
+                <h1 className="text-white text-lg font-bold">グルメサーチ</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="ja">
+      <body>
+       <NavBar />
+       <main>{children}</main>
       </body>
     </html>
   );
