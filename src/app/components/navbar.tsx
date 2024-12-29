21c2ad24f91feae4bdb2c686213ae2e318df8341
仲村 優佳 <ynakamura704@gmail.com>
2024-12-30 01:32:31 +0900

    ロゴでルートページに戻れるようにした

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
-const NavBar: React.FC = () => {
+const NavBar = () => {
     return (
         <nav className = "bg-red-600 p-4">
             <div className = "container mx-auto">
+                <Link href={`/`}>
                 <h1 className="text-white text-lg font-bold">グルメサーチ</h1>
+                </Link>
             </div>
         </nav>
     )
